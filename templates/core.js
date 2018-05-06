fec98c2ce438d018315f38a8d6931a58d5f79964
cburge3 <chip@chipburge.com>
2018-05-06 14:41:27 -0400

    Got C3.js chart working

--- templates/core.js
+++ templates/core.js
@@ -1,23 +1,11 @@
-    var value;
-    var xVal = 0;
-    var updateInterval = 10000;
-/* Periodically refresh incoming data */
-      $(document).ready(function() {
-        window.setInterval(function(){
-          $.get("/generator").done(function(result) {
-           var values = JSON.parse(result);
-            jQuery.each(values, function(i, val) {
-            document.getElementById(i).innerHTML = val;
-            });
-          });
-        }, updateInterval);
-      });
+var values;
+var xVal = 0;
+var updateInterval = 2000;
 
 /* Populate dynamic text for all of the devices */
 	window.onload = function () {
 	    $("h2").each(function(index, error) {
 	        console.log(index+' '+ error);
-	        //alert($(this).text());
             $(this).text($(this).closest("div").attr("id"));
         });
 }
@@ -36,56 +24,33 @@ $("button").click(function(e) {
     });
 });
 
-var n = 40,
-    random = d3.randomNormal(0, .2),
-    data = d3.range(n).map(random);
-var svg = d3.select("svg"),
-    margin = {top: 20, right: 20, bottom: 20, left: 40},
-    width = +svg.attr("width") - margin.left - margin.right,
-    height = +svg.attr("height") - margin.top - margin.bottom,
-    g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-var x = d3.scaleLinear()
-    .domain([0, n - 1])
-    .range([0, width]);
-var y = d3.scaleLinear()
-    .domain([-1, 1])
-    .range([height, 0]);
-var line = d3.line()
-    .x(function(d, i) { return x(i); })
-    .y(function(d, i) { return y(d); });
-g.append("defs").append("clipPath")
-    .attr("id", "clip")
-  .append("rect")
-    .attr("width", width)
-    .attr("height", height);
-g.append("g")
-    .attr("class", "axis axis--x")
-    .attr("transform", "translate(0," + y(0) + ")")
-    .call(d3.axisBottom(x));
-g.append("g")
-    .attr("class", "axis axis--y")
-    .call(d3.axisLeft(y));
-g.append("g")
-    .attr("clip-path", "url(#clip)")
-  .append("path")
-    .datum(data)
-    .attr("class", "line")
-  .transition()
-    .duration(500)
-    .ease(d3.easeLinear)
-    .on("start", tick);
-function tick() {
-  // Push a new data point onto the back.
-  data.push(random());
-  // Redraw the line.
-  d3.select(this)
-      .attr("d", line)
-      .attr("transform", null);
-  // Slide it to the left.
-  d3.active(this)
-      .attr("transform", "translate(" + x(-1) + ",0)")
-    .transition()
-      .on("start", tick);
-  // Pop the old data point off the front.
-  data.shift();
-}
+var v1 = ['TI100'],
+v2 = ['TI101']
+var datapoints = [v1, v2];
+
+var chart = c3.generate({
+    bindto: '#chart',
+    data: {
+      columns: datapoints
+    }
+});
+/* Periodically refresh incoming data */
+      $(document).ready(function() {
+        window.setInterval(function(){
+          $.get("/generator").done(function(result) {
+           values = JSON.parse(result);
+            jQuery.each(values, function(i, val) {
+            document.getElementById(i).innerHTML = val;
+            var l = datapoints.length;
+            for (var j = 0; j < l; j++) {
+                if (datapoints[j][0] == i){
+                    datapoints[j].push(val);
+                }
+            }
+            chart.load({
+                columns: datapoints
+                });
+            });
+          });
+        }, updateInterval);
+      });
